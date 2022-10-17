ad45f2ad669638b2eed64722c767a90a6b2741c7
ttorsteinbo <ttorsteinbo@users.noreply.github.com>
2022-10-17 12:22:40 +0200

    Add secret storage recommendation in code example

--- code-examples/ecom_python_example/config.py
+++ code-examples/ecom_python_example/config.py
@@ -2,13 +2,12 @@ config = {
     "host_server": "<host_address>",
     'token_request':{
         'client_id': '<client_id>',
-        'client_secret': '<client_secret>',
-        'Ocp-Apim-Subscription-Key': '<Ocp-Apim-Subscription-Key> For Access Token',
+        'client_secret': '<client_secret>', # Do not put your secret directly in the source code, consider retrieving it from a secure store
+        'Ocp-Apim-Subscription-Key': '<Ocp-Apim-Subscription-Key> For Access Token', # Do not put your secret key directly in the source code, consider retrieving it from a secure store
     },
     'ecom':{
         'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key': '<Ocp-Apim-Subscription-Key> For the eCommerce product',
-    },
+        'Ocp-Apim-Subscription-Key': '<Ocp-Apim-Subscription-Key> For the eCommerce product', # Do not put your secret key directly in the source code, consider retrieving it from a secure store
     'ecom_initiate_payment_body':{
         "merchantInfo": {
             "merchantSerialNumber": "<merchantSerialNumber>",
